9a1222e2b7b09222ddcd9a2a5457b41881858698
Anteru <andre.shale@yandex.ru>
2023-03-30 18:54:49 +0300

    hotfix: error 404

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const { NOT_FOUND_ERROR } = require('../utils/res-constants');
 routes.use('/users', require('./users'));
 routes.use('/cards', require('./cards'));
 
-routes.use('*', (req, res) => {
+routes.use((req, res) => {
   res.status(NOT_FOUND_ERROR).send({ message: 'Несуществующий маршрут. Ошибка: 404' });
 });
 
